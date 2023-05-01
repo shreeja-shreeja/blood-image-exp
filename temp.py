43e581349031a4da2745d529ab64076e6fae24d3
Shreeja Shreeja <115733593+shreeja-shreeja@users.noreply.github.com>
2023-05-01 22:27:45 +0200

    Update temp.py
    All cells in an image marked using annotations csv

--- temp.py
+++ temp.py
@@ -1,19 +1,36 @@
 import numpy as np
 import cv2
-
+blood = "BloodImage_00022.jpg"
 #img = np.zeros([640, 640, 3])
-img = cv2.imread('/home/sarayu941/blood_imaging/BCCD_Dataset/BCCD/JPEGImages/BloodImage_00017.jpg')
+img = cv2.imread('/home/sarayu941/blood_imaging/BCCD_Dataset/BCCD/JPEGImages/'+blood)
 
+import pandas as pd
 
-tmp = np.ones([176, 2, 3]) * 0
-img[215:391, 126:128, 0:3] = tmp
-tmp = np.ones([2,208,3]) * 0
-img[213:215, 128:336, 0:3] = tmp
-tmp = np.ones([176,2,3]) * 0
-img[215:391, 334:336, 0:3] = tmp
-tmp = np.ones([2,208,3]) * 0
-img[389:391, 128:336, 0:3] = tmp
+df = pd.read_csv('/home/sarayu941/blood_imaging/BCCD_Dataset/test.csv')
+rslt_df = df.loc[df['filename'] == blood]
+rslt_df1 = rslt_df.loc[rslt_df['cell_type'] == "Platelets"]
+
+for ind in rslt_df1.index:
+    xmin = int(rslt_df1["xmin"][ind])
+    xmax = int(rslt_df1["xmax"][ind])
+    ymin = int(rslt_df1["ymin"][ind])
+    ymax = int(rslt_df1["ymax"][ind])
+
+    if (xmin<2):
+        xmin = 2
+    if (ymin<2):
+        ymin = 2
+
+    tmp = np.ones([(ymax-ymin), 2, 3]) * 0
+    img[ymin:ymax, (xmin-2):xmin, 0:3] = tmp
+    tmp = np.ones([2,(xmax-xmin),3]) * 0
+    img[(ymin-2):ymin, xmin:xmax, 0:3] = tmp
+    tmp = np.ones([(ymax-ymin),2,3]) * 0
+    img[ymin:ymax, (xmax-2):xmax, 0:3] = tmp
+    tmp = np.ones([2,(xmax-xmin),3]) * 0
+    img[(ymax-2):ymax, xmin:xmax, 0:3] = tmp
 cv2.imshow('Filtered_original',img)
 cv2.waitKey()
 
 print()
+
