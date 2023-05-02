1f1fcd82d485d072c278ae7ed64d17d6645c5442
Shreeja Shreeja <115733593+shreeja-shreeja@users.noreply.github.com>
2023-05-02 09:11:27 +0200

    Update temp.py
    All annotations marked.

--- temp.py
+++ temp.py
@@ -1,21 +1,8 @@
 import numpy as np
 import cv2
-blood = "BloodImage_00022.jpg"
-#img = np.zeros([640, 640, 3])
-img = cv2.imread('/home/sarayu941/blood_imaging/BCCD_Dataset/BCCD/JPEGImages/'+blood)
-
 import pandas as pd
 
-df = pd.read_csv('/home/sarayu941/blood_imaging/BCCD_Dataset/test.csv')
-rslt_df = df.loc[df['filename'] == blood]
-rslt_df1 = rslt_df.loc[rslt_df['cell_type'] == "Platelets"]
-
-for ind in rslt_df1.index:
-    xmin = int(rslt_df1["xmin"][ind])
-    xmax = int(rslt_df1["xmax"][ind])
-    ymin = int(rslt_df1["ymin"][ind])
-    ymax = int(rslt_df1["ymax"][ind])
-
+def smear(xmin,ymin,xmax,ymax,img,type,color):
     if (xmin<2):
         xmin = 2
     if (ymin<2):
@@ -29,8 +16,37 @@ for ind in rslt_df1.index:
     img[ymin:ymax, (xmax-2):xmax, 0:3] = tmp
     tmp = np.ones([2,(xmax-xmin),3]) * 0
     img[(ymax-2):ymax, xmin:xmax, 0:3] = tmp
+
+    cv2.putText(img, type, (xmin + 10, ymin + 15),
+							cv2.FONT_HERSHEY_SIMPLEX, 1e-3 * img.shape[0], color, 2)
+    return 
+
+blood = "BloodImage_00123.jpg"
+#img = np.zeros([640, 640, 3])
+img = cv2.imread('/home/sarayu941/blood_imaging/BCCD_Dataset/BCCD/JPEGImages/'+blood)
+
+df = pd.read_csv('/home/sarayu941/blood_imaging/BCCD_Dataset/test.csv')
+rslt_df = df.loc[df['filename'] == blood]
+celltypes = ["RBC", "WBC", "Platelets"]
+colors = [(0,0,255),(255,0,0),(0,0,0)]
+
+for i,name in enumerate(celltypes):
+    rslt_df1 = rslt_df.loc[rslt_df['cell_type'] == name]
+
+    for ind in rslt_df1.index:
+        xmin = int(rslt_df1["xmin"][ind])
+        xmax = int(rslt_df1["xmax"][ind])
+        ymin = int(rslt_df1["ymin"][ind])
+        ymax = int(rslt_df1["ymax"][ind])
+        smear(xmin,ymin,xmax,ymax,img,type=name,color=colors[i])
+
 cv2.imshow('Filtered_original',img)
 cv2.waitKey()
 
 print()
+
+
+
+
+
 
