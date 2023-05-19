c9dce428a15ae659a561cf8c039cc31da98f2c32
Shreeja <sarayu941@gmail.com>
2023-05-19 20:55:36 +0200

    Added notebook

--- temp.py
+++ temp.py
@@ -1,20 +1,5 @@
 import numpy as np
 import cv2
-<<<<<<< HEAD
-
-#img = np.zeros([640, 640, 3])
-img = cv2.imread('/home/sarayu941/blood_imaging/BCCD_Dataset/BCCD/JPEGImages/BloodImage_00017.jpg')
-
-
-tmp = np.ones([176, 2, 3]) * 0
-img[215:391, 126:128, 0:3] = tmp
-tmp = np.ones([2,208,3]) * 0
-img[213:215, 128:336, 0:3] = tmp
-tmp = np.ones([176,2,3]) * 0
-img[215:391, 334:336, 0:3] = tmp
-tmp = np.ones([2,208,3]) * 0
-img[389:391, 128:336, 0:3] = tmp
-=======
 import pandas as pd
 
 def smear(xmin,ymin,xmax,ymax,img,type,color):
@@ -55,17 +40,13 @@ for i,name in enumerate(celltypes):
         ymax = int(rslt_df1["ymax"][ind])
         smear(xmin,ymin,xmax,ymax,img,type=name,color=colors[i])
 
->>>>>>> 4a932583ddb69fe2b7fa103c88f6503fdbc4ef45
 cv2.imshow('Filtered_original',img)
 cv2.waitKey()
 
 print()
-<<<<<<< HEAD
-=======
 
 
 
 
 
 
->>>>>>> 4a932583ddb69fe2b7fa103c88f6503fdbc4ef45
